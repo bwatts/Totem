b1ce2807a239ad4889fafeae044d7efc06bb9b4d
Bryan Watts <bwatts@gmail.com>
2016-02-17 11:35:34 -0500

    Remove unnecessary disposal of bounded observable

--- Source/Totem.Runtime/Timeline/TimelineSchedule.cs
+++ Source/Totem.Runtime/Timeline/TimelineSchedule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 
 namespace Totem.Runtime.Timeline
@@ -10,82 +11,69 @@ namespace Totem.Runtime.Timeline
 	/// </summary>
 	internal sealed class TimelineSchedule : PushScope
 	{
-    private readonly HashSet<IDisposable> _timers = new HashSet<IDisposable>();
-    private readonly HashSet<TimelinePosition> _resumedPositions = new HashSet<TimelinePosition>();
+		private readonly HashSet<TimelinePosition> _resumedPositions = new HashSet<TimelinePosition>();
 		private readonly TimelineScope _scope;
-    private TimelinePosition _resumeCheckpoint;
+		private TimelinePosition _resumeCheckpoint;
 
 		internal TimelineSchedule(TimelineScope scope)
 		{
 			_scope = scope;
 		}
 
-    internal void ResumeWith(ResumeInfo info)
-    {
-      foreach(var pointInfo in info.Points)
-      {
-        if(pointInfo.OnSchedule)
-        {
-          _resumedPositions.Add(pointInfo.Point.Position);
-        }
+		internal void ResumeWith(ResumeInfo info)
+		{
+			foreach(var pointInfo in info.Points)
+			{
+				if(pointInfo.OnSchedule)
+				{
+					_resumedPositions.Add(pointInfo.Point.Position);
+				}
 
-        _resumeCheckpoint = pointInfo.Point.Position;
-      }
-    }
+				_resumeCheckpoint = pointInfo.Point.Position;
+			}
+		}
 
-    protected override void Close()
-    {
-      base.Close();
+		protected override void Close()
+		{
+			base.Close();
 
-      foreach(var timer in _timers.RemoveAll())
-      {
-        timer.Dispose();
-      }
-
-      _resumedPositions.Clear();
-    }
+			_resumedPositions.Clear();
+		}
 
 		protected override void Push()
 		{
-      if(Point.Scheduled && CanPush())
-      {
-        AddTimer();
-      }
+			if(Point.Scheduled && CanPush())
+			{
+				AddTimer();
+			}
 		}
 
-    private bool CanPush()
-    {
-      return _resumedPositions.Remove(Point.Position) || Point.Position > _resumeCheckpoint;
-    }
+		private bool CanPush()
+		{
+			return _resumedPositions.Remove(Point.Position) || Point.Position > _resumeCheckpoint;
+		}
 
 		private void AddTimer()
 		{
-      var point = Point;
+			var point = Point;
 
-      IDisposable timer = null;
-
-      timer = Observable
-        .Timer(new DateTimeOffset(point.Event.When))
-        .Take(1)
-        .Subscribe(_ => Push(point, timer));
-
-      _timers.Add(timer);
+			Observable
+				.Timer(new DateTimeOffset(point.Event.When))
+				.Take(1)
+				.ObserveOn(ThreadPoolScheduler.Instance)
+				.Subscribe(_ => Push(point));
 		}
 
-    private void Push(TimelinePoint point, IDisposable timer)
-    {
-      if(State.IsConnecting || State.IsConnected)
-      {
-        _timers.Remove(timer);
-
-        timer.Dispose();
-
-        _scope.PushScheduled(point);
-      }
-      else
-      {
-        Log.Warning("[timeline] Cannot push to schedule when {Phase:l} - ignoring {Point:l}", State.Phase, point);
-      }
-    }
-  }
+		private void Push(TimelinePoint point)
+		{
+			if(State.IsConnecting || State.IsConnected)
+			{
+				_scope.PushScheduled(point);
+			}
+			else
+			{
+				Log.Warning("[timeline] Cannot push to schedule when {Phase:l} - ignoring {Point:l}", State.Phase, point);
+			}
+		}
+	}
 }
