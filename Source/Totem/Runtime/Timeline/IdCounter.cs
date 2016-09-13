2a46ef5cad9c69d6d9a0c2dbed84499c1fed1cce
edamtoft <edamtoft@dealeron.com>
2016-09-13 10:47:39 -0400

    IdCounter class will serialize better for snapshotting

--- Source/Totem/Runtime/Timeline/IdCounter.cs
+++ Source/Totem/Runtime/Timeline/IdCounter.cs
@@ -7,6 +7,7 @@ namespace Totem.Runtime.Timeline
   /// <summary>
   /// Provides an incremental series of integers
   /// </summary>
+  [Durable]
   public sealed class IdCounter
   {
     private long _value;
@@ -14,21 +15,16 @@ namespace Totem.Runtime.Timeline
     public IdCounter(long value = 0)
     {
       _value = value;
-
-      Current = Id.From(_value);
-      Next = Id.From(_value + 1);
     }
 
-    public Id Current { get; private set; }
-    public Id Next { get; private set; }
+    [Transient]
+    public Id Current => Id.From(_value);
+    [Transient]
+    public Id Next => Id.From(_value + 1);
 
     public void MoveNext()
     {
-      Current = Next;
-
       _value += 1;
-
-      Next = Id.From(_value + 1);
     }
 
     public override string ToString() => Current.ToString();
