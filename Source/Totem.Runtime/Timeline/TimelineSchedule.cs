110044417a09f020416617fda977a168b88eb502
edamtoft <edamtoft@dealeron.com>
2017-02-09 17:15:47 -0500

    Remove Scheduling Race Condition

--- Source/Totem.Runtime/Timeline/TimelineSchedule.cs
+++ Source/Totem.Runtime/Timeline/TimelineSchedule.cs
@@ -1,55 +1,36 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 
 namespace Totem.Runtime.Timeline
 {
-	/// <summary>
-	/// Schedules points on the timeline to occur in the future
-	/// </summary>
-	internal sealed class TimelineSchedule : Connection
-	{
-    readonly ConcurrentDictionary<IDisposable, bool> _timers = new ConcurrentDictionary<IDisposable, bool>();
+  /// <summary>
+  /// Schedules points on the timeline to occur in the future
+  /// </summary>
+  internal sealed class TimelineSchedule : Connection
+  {
     readonly TimelineScope _timeline;
 
     internal TimelineSchedule(TimelineScope timeline)
-		{
-			_timeline = timeline;
-		}
-
-    protected override void Close()
     {
-      foreach(var timer in _timers.Keys)
-      {
-        timer.Dispose();
-      }
+      _timeline = timeline;
     }
 
     internal void Push(TimelineMessage message)
-		{
+    {
       var timer = null as IDisposable;
 
       timer = Observable
-				.Timer(new DateTimeOffset(message.Point.Event.When))
-				.Take(1)
-				.Subscribe(_ => PushToTimeline(message, timer));
-
-      _timers[timer] = true;
-		}
+        .Timer(new DateTimeOffset(message.Point.Event.When))
+        .Take(1)
+        .Subscribe(_ => PushToTimeline(message, timer));
+    }
 
     void PushToTimeline(TimelineMessage message, IDisposable timer)
     {
       try
       {
-        bool ignored;
-
-        _timers.TryRemove(timer, out ignored);
-
-        timer.Dispose();
-
+        timer?.Dispose();
         _timeline.PushFromSchedule(message);
       }
       catch(Exception error)
@@ -57,5 +38,5 @@ namespace Totem.Runtime.Timeline
         Log.Error(error, "[timeline] Failed to push scheduled event of type {EventType}. The timeline will attempt to push it again after a restart.", message.Point.EventType);
       }
     }
-	}
+  }
 }
