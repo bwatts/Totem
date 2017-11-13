6ec4847e36f121488a253320b001d20069cbe9f9
Tom Warner <tomwarner13@gmail.com>
2017-11-13 09:20:25 -0700

    Fix class name

--- Source/Totem/Runtime/OutputAttribute.cs
+++ Source/Totem/Runtime/OutputAttribute.cs
@@ -10,7 +10,7 @@ namespace Totem.Runtime
   /// Indicates the decorated type will be serialized as JSON, but never deserialized.
   /// Useful if the property always needs to be set through code.
   /// </summary>
-  public sealed class WriteOnlyAttribute : Attribute
+  public sealed class OutputAttribute : Attribute
   {
 
   }
