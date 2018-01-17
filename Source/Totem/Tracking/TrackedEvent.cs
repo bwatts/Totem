c457ecf07a29718acca8c18098af9f504df48949
Bryan Watts <bwatts@gmail.com>
2018-01-17 16:25:09 -0500

    Add missing constructor for derived tracking events

--- Source/Totem/Tracking/TrackedEvent.cs
+++ Source/Totem/Tracking/TrackedEvent.cs
@@ -5,23 +5,26 @@ namespace Totem.Tracking
   /// <summary>
   /// A timeline event tracked by an index
   /// </summary>
-	public class TrackedEvent
-	{
-		public TrackedEvent(string eventType, long eventPosition, Id userId, DateTime eventWhen, string keyType, string keyValue)
-		{
-			EventType = eventType;
-			EventPosition = eventPosition;
-			UserId = userId;
-			EventWhen = eventWhen;
-			KeyType = keyType;
-			KeyValue = keyValue;
-		}
+  public class TrackedEvent
+  {
+    protected TrackedEvent()
+    {}
 
-		public string EventType;
-		public long EventPosition;
-		public Id UserId;
-		public DateTime EventWhen;
-		public string KeyType;
-		public string KeyValue;
-	}
+    public TrackedEvent(string eventType, long eventPosition, Id userId, DateTime eventWhen, string keyType, string keyValue)
+    {
+      EventType = eventType;
+      EventPosition = eventPosition;
+      UserId = userId;
+      EventWhen = eventWhen;
+      KeyType = keyType;
+      KeyValue = keyValue;
+    }
+
+    public string EventType;
+    public long EventPosition;
+    public Id UserId;
+    public DateTime EventWhen;
+    public string KeyType;
+    public string KeyValue;
+  }
 }
