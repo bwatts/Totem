9bf69d03b9b2bb2883c3e2061f21bca80a416d63
Bryan Watts <bwatts@gmail.com>
2021-05-17 22:26:25 -0400

    Destructure logs using short ids and Totem type names

---
+++ src/Totem.AspNetCore/Hosting/SerilogExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using Serilog;
+using Serilog.Configuration;
+using Totem.Events;
+
+namespace Totem.Hosting
+{
+    public static class SerilogExtensions
+    {
+        public static LoggerConfiguration ShortIds(this LoggerDestructuringConfiguration destructure)
+        {
+            if(destructure == null)
+                throw new ArgumentNullException(nameof(destructure));
+
+            return destructure.ByTransforming<Id>(id => id.ToShortString());
+        }
+
+        public static LoggerConfiguration WithShortTypes(this LoggerEnrichmentConfiguration enrich, Func<Type, bool> useShortType)
+        {
+            if(enrich == null)
+                throw new ArgumentNullException(nameof(enrich));
+
+            return enrich.With(new ShortTypeEnricher(useShortType));
+        }
+
+        public static LoggerConfiguration WithShortTotemTypes(this LoggerEnrichmentConfiguration enrich) =>
+            enrich.WithShortTypes(type => typeof(IMessage).IsAssignableFrom(type) || typeof(IEventSourced).IsAssignableFrom(type));
+    }
+}
---
+++ src/Totem.AspNetCore/Hosting/ShortTypeEnricher.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Totem.Hosting
+{
+    public class ShortTypeEnricher : ILogEventEnricher
+    {
+        readonly Func<Type, bool> _useShortType;
+
+        public ShortTypeEnricher(Func<Type, bool> useShortType) =>
+            _useShortType = useShortType ?? throw new ArgumentNullException(nameof(useShortType));
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if(logEvent == null)
+                throw new ArgumentNullException(nameof(logEvent));
+
+            if(propertyFactory == null)
+                throw new ArgumentNullException(nameof(propertyFactory));
+
+            List<LogEventProperty>? updates = null;
+
+            foreach(var property in logEvent.Properties)
+            {
+                if(TryGetShortType(property, out var shortTypeProperty))
+                {
+                    updates ??= new List<LogEventProperty>();
+                    updates.Add(shortTypeProperty);
+                }
+            }
+
+            if(updates != null)
+            {
+                foreach(var update in updates)
+                {
+                    logEvent.AddOrUpdateProperty(update);
+                }
+            }
+        }
+
+        bool TryGetShortType(KeyValuePair<string, LogEventPropertyValue> property, out LogEventProperty shortTypeProperty)
+        {
+            if(property.Value is ScalarValue { Value: Type type2 })
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+
+            if(property.Value is ScalarValue { Value: Type type } && _useShortType(type))
+            {
+                shortTypeProperty = new LogEventProperty(property.Key, new ScalarValue(type.Name));
+                return true;
+            }
+
+            shortTypeProperty = null!;
+            return false;
+        }
+    }
+}
