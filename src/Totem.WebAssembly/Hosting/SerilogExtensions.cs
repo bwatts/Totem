16e9aeba8ebde10964a5f6dbb75edd46f4fc6530
Bryan Watts <bwatts@gmail.com>
2021-06-01 23:10:47 -0400

    Add Totem.WebAssembly project

--- src/Totem.WebAssembly/Hosting/SerilogExtensions.cs
+++ src/Totem.WebAssembly/Hosting/SerilogExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Serilog;
 using Serilog.Configuration;
+using Totem.Core;
 using Totem.Events;
 
 namespace Totem.Hosting
---
+++ src/Totem.WebAssembly/Hosting/WebAssemblyHostBuilderExtensions.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Hosting;
+using Serilog.Extensions.Logging;
+
+namespace Totem.Hosting
+{
+    public static class WebAssemblyHostBuilderExtensions
+    {
+        public static WebAssemblyHostBuilder UseBaseHttpClient(this WebAssemblyHostBuilder builder)
+        {
+            if(builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            builder.Services.TryAddScoped(_ => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            return builder;
+        }
+
+        public static WebAssemblyHostBuilder UseRootComponent<TComponent>(this WebAssemblyHostBuilder builder, string selector)
+            where TComponent : IComponent
+        {
+            if(builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            builder.RootComponents.Add<TComponent>(selector);
+
+            return builder;
+        }
+
+        public static WebAssemblyHostBuilder UseSerilog(this WebAssemblyHostBuilder builder, Action<LoggerConfiguration>? configure = null)
+        {
+            if(builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            // Adapted from https://github.com/serilog/serilog-aspnetcore/blob/dev/src/Serilog.AspNetCore/SerilogWebHostBuilderExtensions.cs
+
+            var configuration = new LoggerConfiguration();
+
+            configure?.Invoke(configuration);
+
+            var logger = configuration.CreateLogger();
+            builder.Services.AddSingleton(logger);
+            builder.Services.AddSingleton<ILoggerFactory>(new SerilogLoggerFactory(logger));
+
+            var diagnosticContext = new DiagnosticContext(logger);
+            builder.Services.AddSingleton(diagnosticContext);
+            builder.Services.AddSingleton<IDiagnosticContext>(diagnosticContext);
+
+            return builder;
+        }
+    }
+}
